a82cc529829d470fd59b53e70eec1c54182b862c
text name <edgar.jin88@gmail.com>
2020-04-07 00:00:31 +1000

    tracing log for ec2 property

--- handler.js
+++ handler.js
@@ -1,18 +1,25 @@
-'use strict';
+"use strict";
 
-module.exports.hello = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+// To find all ec2 on a accountId regarless of regions.
+// To find the attached service group of each ec2.
+//To test with Jest and mock api
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+//to run on VPC
+//to attach CloudWatch
+const AWS = require("aws-sdk");
+AWS.config.region = "us-east-2";
+
+module.exports.listEC2Instances = async (event) => {
+  let ec2 = new AWS.EC2();
+  let result = (await ec2)
+    ? ec2.describeInstances()
+    : "Unable to get ec2 instances";
+  console.log("ec2 instance list", result);
+  // return {
+  //   statusCode: 200,
+  //   body: JSON.stringify({
+  //     message: "ec2 list here. ",
+  //     instanceList: result,
+  //   }),
+  // };
 };
