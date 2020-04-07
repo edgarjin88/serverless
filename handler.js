97c81b234d193f76f78465617f7dcb7520a0416a
text name <edgar.jin88@gmail.com>
2020-04-07 10:34:18 +1000

    Sudo code

--- handler.js
+++ handler.js
@@ -1,11 +1,22 @@
 "use strict";
 
-// To find all ec2 on a accountId regarless of regions.
-// To find the attached service group of each ec2.
-//To test with Jest and mock api
+// 1. To find all ec2 on a accountId regarless of regions. //because some security groups may not be used
+// 2. To find the attached service group of each ec2.
 
-//to run on VPC
-//to attach CloudWatch
+// Find and checkout related AWS api first to chase up user and EC2 info.
+// Use attach CloudWatch
+
+//Options-
+//to run on VPC? if required?
+// to add specific policies as well.
+
+//Refactoring
+// to use try-catch block for async.
+// heavy logic to be out of handler
+
+//Testing
+//To test with Jest and mock api and AWS
+
 const AWS = require("aws-sdk");
 AWS.config.region = "us-east-2";
 
