51b7e3d8d287428ea29606d5e2795a7002a94783
text name <edgar.jin88@gmail.com>
2020-04-08 13:05:14 +1000

    policy updated in YML file and minor refectoring

--- ec2.js
+++ ec2.js
@@ -10,53 +10,59 @@ var securityParams = {};
 //regionParams and securityParam info not required at this stage.
 
 const getAllRegions = async () => {
-  const regionNameList = [];
-  const regions = await ec2.describeRegions(regionParams).promise();
-  regions.Regions.forEach((el) => {
-    regionNameList.push(el.RegionName);
-  });
-  return regionNameList;
+  try {
+    const regionNameList = [];
+    const regions = await ec2.describeRegions(regionParams).promise();
+    regions.Regions.forEach((el) => {
+      regionNameList.push(el.RegionName);
+    });
+    return regionNameList;
+  } catch (e) {
+    console.log("error in getAllRegions function :", e);
+  }
 };
 
 module.exports = async () => {
-  const regionList = await getAllRegions();
+  try {
+    const regionList = await getAllRegions();
 
-  let finalInstanceList = regionList.reduce(async (acc, currentName) => {
-    let ec2 = new AWS.EC2({ apiVersion: "2016-11-15", region: currentName });
+    let finalInstanceList = regionList.reduce(async (acc, currentName) => {
+      let ec2 = new AWS.EC2({ apiVersion: "2016-11-15", region: currentName });
 
-    const eachRegion = {
-      region_name: currentName,
-      instance_info: [],
-    };
+      const eachRegion = {
+        region_name: currentName,
+        instance_info: [],
+      };
 
-    const description = await ec2.describeInstances(params).promise();
-    const { SecurityGroups } = await ec2
-      .waitFor("securityGroupExists", securityParams)
-      .promise();
+      const description = await ec2.describeInstances(params).promise();
+      const { SecurityGroups } = await ec2
+        .waitFor("securityGroupExists", securityParams)
+        .promise();
 
-    // if EC2s instance exist, update region information.
-    if (description.Reservations && description.Reservations.length > 0) {
-      // All available security groups in a region with actuall EC2
-      eachRegion.available_security_groups = SecurityGroups;
+      // if EC2s instance exist, update region information.
+      if (description.Reservations && description.Reservations.length > 0) {
+        // All available security groups in a region with actuall EC2
+        eachRegion.available_security_groups = SecurityGroups;
 
-      description.Reservations.forEach((el) => {
-        el.Instances.forEach((innerInstance) => {
-          let tempObj = {};
-          tempObj.InstanceId = innerInstance.InstanceId;
+        description.Reservations.forEach((el) => {
+          el.Instances.forEach((innerInstance) => {
+            let tempObj = {};
+            tempObj.InstanceId = innerInstance.InstanceId;
 
-          //Scurity group that an EC2 actually uses
-          tempObj.SecurityGroups = innerInstance.SecurityGroups;
-          eachRegion.instance_info.push(tempObj);
+            //Scurity group that an EC2 actually uses
+            tempObj.SecurityGroups = innerInstance.SecurityGroups;
+            eachRegion.instance_info.push(tempObj);
+          });
         });
-      });
-    }
+      }
 
-    const addedInfo = await acc;
-    eachRegion.instance_info.length > 0 ? addedInfo.push(eachRegion) : null;
+      const addedInfo = await acc;
+      eachRegion.instance_info.length > 0 ? addedInfo.push(eachRegion) : null;
 
-    return Promise.all(addedInfo);
-  }, []);
-  return finalInstanceList;
+      return Promise.all(addedInfo);
+    }, []);
+    return finalInstanceList;
+  } catch (e) {
+    console.log("error in accessInstanceList function :", e);
+  }
 };
-
-// func1().then((data) => console.log("shit :", data));
--- handler.js
+++ handler.js
@@ -23,14 +23,18 @@
 const accessInstanceList = require("./ec2");
 
 module.exports.listEC2Instances = async (event) => {
-  const result = await accessInstanceList();
-  console.log("returned result :", result);
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message:
-        "Actuall EC2 instances in all regions and the secuity groups of each EC2 instance",
-      available_regions_with_EC2Intances: result,
-    }),
-  };
+  try {
+    const result = await accessInstanceList();
+    console.log("returned result :", result);
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message:
+          "Actuall EC2 instances in all regions and the secuity groups of each EC2 instance",
+        available_regions_with_EC2Intances: result,
+      }),
+    };
+  } catch (e) {
+    console.log("error in listEC2Instances function :", e);
+  }
 };
